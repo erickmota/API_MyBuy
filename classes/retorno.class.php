77d01646381a875899babad1924809fff2a212f3
Erick <erick_fcsaopaulo@hotmail.com>
2024-04-14 14:47:35 -0300

    Primeiro retorno

--- classes/retorno.class.php
+++ classes/retorno.class.php
@@ -2,6 +2,75 @@
 
 class Retornos{
 
+    public function retornarAll($tabela){
 
+        include 'conexao.class.php';
+
+        $sql = mysqli_query($conn, "SELECT * FROM $tabela") or die("Erro mostrar");
+        $qtd = mysqli_num_rows($sql);
+        while ($row = mysqli_fetch_assoc($sql)){
+                
+            $array[] = $row;
+            
+        }
+
+        if($qtd < 1){
+
+            return json_encode([
+
+                "status" => API_IS_ACTIVE,
+                "Versao" => API_VERSION,
+                "msg" => "Sucesso",
+                "data" => false
+        
+            ]);
+        
+
+        }else{
+
+            return json_encode([
+
+                "status" => API_IS_ACTIVE,
+                "Versao" => API_VERSION,
+                "msg" => "Sucesso",
+                "data" => $array
+        
+            ]);
+
+        }
+
+    }
+
+    public function retornaDado($tabela, $coluna, $dado){
+
+        include 'conexao.class.php';
+
+        $sql = mysqli_query($conn, "SELECT * FROM $tabela WHERE $coluna='$dado'") or die ("Erro BD");
+        $qtd = mysqli_num_rows($sql);
+        while ($row = mysqli_fetch_assoc($sql)){
+                
+            $array[] = $row;
+            
+        }
+
+        if($qtd < 1){
+
+            return false;
+        
+
+        }else{
+
+            return json_encode([
+
+                "status" => API_IS_ACTIVE,
+                "Versao" => API_VERSION,
+                "msg" => "Sucesso",
+                "data" => $array
+        
+            ]);
+
+        }
+
+    }
 
 }
--- index.php
+++ index.php
@@ -1,2 +1,53 @@
 <?php
 header('Content-Type: application/json');
+
+include "config.php";
+
+include "classes/retorno.class.php";
+$classeRetorno = new Retornos();
+
+if(isset($_GET["url"])){
+
+    $explode = explode("/", $_GET["url"]);
+
+    $headers = apache_request_headers();
+
+    /* Verificando se a API está ativada, e se o token de verificação existe
+    e é válido. */
+    if(API_IS_ACTIVE == true && isset($headers["API_TOKEN"]) && $headers['API_TOKEN'] == API_TOKEN){
+
+        switch($explode[0]){
+
+            case "produtos";
+
+                echo $classeRetorno->retornarAll("produtos");
+
+            break;
+
+        }
+
+    }else{
+
+        echo json_encode([
+    
+            "status" => API_IS_ACTIVE,
+            "Versao" => API_VERSION,
+            "msg" => "Dados de verificação não conferem, ou existe algum erro interno",
+            "data" => false
+    
+        ]);
+
+    }
+
+}else{
+
+    echo json_encode([
+    
+        "status" => API_IS_ACTIVE,
+        "Versao" => API_VERSION,
+        "msg" => "URL da API não existente",
+        "data" => false
+
+    ]);
+
+}
