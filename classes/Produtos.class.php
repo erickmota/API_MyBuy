bc723c116efe28cb5d24c7ca2d07c7883105a4f5
Erick <erick_fcsaopaulo@hotmail.com>
2024-09-14 00:03:15 -0300

    adicionado retorno da quantidade dos produtos na lista

--- classes/Produtos.class.php
+++ classes/Produtos.class.php
@@ -84,7 +84,7 @@ class Produtos extends Usuarios{
 
                 $sql = mysqli_query(
             
-                    $conn, "SELECT produtos.id, produtos.nome, produtos.carrinho, fotos.url FROM produtos
+                    $conn, "SELECT produtos.id, produtos.nome, produtos.carrinho, produtos.qtd, fotos.url FROM produtos
                     LEFT JOIN fotos ON fotos.id=produtos.id_fotos
                     INNER JOIN listas ON listas.id=produtos.id_listas
                     INNER JOIN usuarios_listas ON usuarios_listas.id_listas=listas.id
@@ -100,7 +100,7 @@ class Produtos extends Usuarios{
 
                 $sql = mysqli_query(
             
-                    $conn, "SELECT produtos.id, produtos.nome, produtos.carrinho, fotos.url FROM produtos
+                    $conn, "SELECT produtos.id, produtos.nome, produtos.carrinho, produtos.qtd, fotos.url FROM produtos
                     LEFT JOIN fotos ON fotos.id=produtos.id_fotos
                     INNER JOIN listas ON listas.id=produtos.id_listas
                     INNER JOIN usuarios_listas ON usuarios_listas.id_listas=listas.id
